b8bba61c9eb9413381b17c418543974237c25205
Olajire Gershom <olajiregershom777@gmail.com>
2026-07-17 12:16:19 +0100

    Update RNA-seq_analysis script

--- README.md
+++ README.md
@@ -11,3 +11,18 @@ Dataset_description: The effect of upper-respiratory infection on transcriptomic
 Number of samples: 45 samples
 Sequencing type: Expression profiling by high throughput sequencing
 Organism: Mus musculus
+
+Assignment 5
+Total number of genes tested [after filtering]: 29722
+FluD8 vs Control
+Significant upregulated genes: 37
+Significant downregulated genes: 36
+
+FluD4 vs Control
+Significant upregulated genes: 17
+Significant downregulated genes: 23
+
+FluD8 vs FluD4
+Significant upregulated genes: 1
+Significant downregulated genes: 0
+
--- RNA-seq_analysis.R
+++ RNA-seq_analysis.R
@@ -73,3 +73,64 @@ keep <- rowSums(counts(dds)) >= 10
 dds <- dds[keep, ]
 
 cat("Genes after filtering:", nrow(dds), "\n")
+
+#Step 6: Run DESeq2
+dds <- DESeq(dds)
+
+#Step 7: Extract all results--pair wise comparisons
+#FluD4 vs Control
+res_D4 <- results(dds,
+                  contrast = c("Condition", "FluD4", "Control"),
+                  alpha = 0.05)
+#FluD8 vs Control
+res_D8 <- results(dds,
+                  contrast = c("Condition", "FluD8", "Control"),
+                  alpha = 0.05)
+#FluD8 vs FluD4
+res_D8vsD4 <- results(dds,
+                  contrast = c("Condition", "FluD8", "FluD4"),
+                  alpha = 0.05)
+
+cat("≡ FluD4 vs Control ≡\n"); summary(res_D4)
+cat("≡ FluD8 vs Control ≡\n"); summary(res_D8)
+cat("≡ FluD8 vs FluD4 ≡\n"); summary(res_D8vsD4)
+
+#Step 8: LFC shrinkage and export for each comparison
+resultsNames(dds)  #run this first to see exact coefficient names
+shrink_and_export <- function(dds, contrast, filename_prefix) {
+  #Shrinkage using ashr for contrasts
+  res_s <- lfcShrink(dds,
+                     contrast = contrast,
+                     type = "ashr")
+  #Full results Table
+  df_all <- as.data.frame(res_s) %>%
+    tibble::rownames_to_column("gene_id") %>%
+    arrange(padj)
+  
+  write_tsv(
+    df_all,
+    paste0("C:/Users/GERSHOM/Desktop/data/rnaseq/applied-genomics/data", filename_prefix, "all_genes.tsv")
+  )
+  
+  #Significant DEGs
+  df_sig <- df_all %>%
+    filter(!is.na(padj), padj < 0.05, abs(log2FoldChange) > 1)
+  
+  write_tsv(
+    df_sig,
+    paste0("C:/Users/GERSHOM/Desktop/data/rnaseq/applied-genomics/data", filename_prefix, "Sig_DEGs.tsv")
+  )
+  
+  cat(filename_prefix, "significant DEGs:", nrow(df_sig),
+      "(up:", sum(df_sig$log2FoldChange > 0),
+      "|down:", sum(df_sig$log2FoldChange < 0), ")\n")
+  return(invisible(df_all))
+}
+
+#Run for each comparison
+shrink_and_export(dds, c("Condition", "FluD4", "Control"), "FluD4vsControl")
+shrink_and_export(dds, c("Condition", "FluD8", "Control"), "FluD8vsControl")
+shrink_and_export(dds, c("Condition", "FluD8", "FluD4"), "FluD8vsFluD4")
+
+saveRDS(dds, "C:/Users/GERSHOM/Desktop/data/rnaseq/applied-genomics/data/dds_object.rds")
+cat("Analysis complete! \n")
